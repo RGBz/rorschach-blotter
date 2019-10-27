df46c1b6327a5b7d6179de3d707c6e3b469857dd
Anton Beza <anton.beza@gmail.com>
2019-10-27 03:34:53 -0400

    Made it actually generate ink blots

--- rorschach.js
+++ rorschach.js
@@ -1,240 +1,83 @@
 /**
- * Generates a Rorschach image in an HTML5 canvas
- * 
- * @author RGBz
+ * Renders a Rorschach ink blot into a canvas
+ * @param {HTMLCanvasElement} canvas
+ * @param {object} [options]
+ * @param {number} [options.blots] how many ink blots to render
+ * @param {number} [options.iterations] how many passes to let the ink bleed
  */
+function renderRorschachInkBlot (canvas, {
+  blots = randInt(10, 100),
+  iterations = 50
+} = {}) {
+  const width = canvas.width * 0.525; // A little bigger than half the width so the mirroring looks good
+  const height = canvas.height;
 
-/**
- * Generates a random integer within a range
- *
- * @param min the smallest number the random number can be
- * @param max the largest number the random number can be
- * @returns a random integer between min and max
- */
-function randomRange(min, max) {
-    return Math.floor((Math.random() * (max - min)) + min);
-}
+  // Initialize an empty grid of pixels for half the scene
+  const pixels = new Array(height).fill(true).map(() => new Array(width).fill(0));
 
-/**
- * Generates a random integer
- *
- * @param max the maximum integer that can be returned
- * @returns an integer between 0 and max
- */
-function randomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-/**
- * Represents an RGB color
- *
- * @param red the red component of the color (0 to 255)
- * @param green the green component of the color (0 to 255)
- * @param blue the blue component of the color (0 to 255)
- */
-function Color(red, green, blue) {
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
-}
-
-/**
- * Set the red part of the color
- * 
- * @param red the red part of the color between 255 and 0
- */
-Color.prototype.setRed = function(red) {
-    this.red = (red > 0) ? ((red < 255) ? red : 255) : 0;
-}
-
-/**
- * Set the green part of the color
- * 
- * @param green the green part of the color between 255 and 0
- */
-Color.prototype.setGreen = function(green) {
-    this.green = (green > 0) ? ((green < 255) ? green : 255) : 0;
-}
-
-/**
- * Set the blue part of the color
- * 
- * @param blue the blue part of the color between 255 and 0
- */
-Color.prototype.setBlue = function(blue) {
-    this.blue = (blue > 0) ? ((blue < 255) ? blue : 255) : 0;
-}
-
-/**
- * Clones a color
- * 
- * @returns a clone of this color
- */
-Color.prototype.clone = function() {
-    return new Color(this.red, this.green, this.blue);
-}
-
-/**
- * Find the difference (distance between color coordinates)
- * 
- * @param color a color to compare against
- * 
- * @returns an integer representing the non-square-rooted
- * difference between two colors
- */
-Color.prototype.diff = function(color) {
-    var r = this.red - color.red;
-    var g = this.green - color.green;
-    var b = this.blue - color.blue;
-    
-    var nonSquareRootedDistance = (r * r) + (g * g) + (b * b);
-        
-    return nonSquareRootedDistance;
-}
-
-/**
- * Returns a string version of the color
- * 
- * @returns String version of the color
- */
-Color.prototype.toString = function() {
-    return "rgba(" + this.red + "," + this.green + "," + this.blue + ",0.6666)";
-}
-
-/**
- * Represents a brush
- *
- * @param width the width of the brush
- * @param height the height of the brush
- * @param color the color of the brush
- */
-function Brush(width, height, color) {
-    this.color = color;
-    this.width = width;
-    this.height = height;
-}
-
-/**
- * Represents an painter tool that can be used to create
- * Rorschach ink blot images using an HTML5 canvas element
- */
-function RorschachPainter() {
-    
-    // Default the palette to contain only black
-    this.palette = [new Color(0, 0, 0)];
-    
-    // Default the range of ink blobs to be drawn
-    this.inkAmountMin = 1000;
-    this.inkAmountMax = 5000;
-    
-    // Default the maximum blob dimensions
-    this.blobWidthMax = 10;
-    this.blobHeightMax = 10;
-}
-
-/**
- * Sets the palette to be used in the ink blot
- * 
- * @param palette an array of Color objects
- */
-RorschachPainter.prototype.setPalette = function(palette) {
-    this.palette = palette;
-}
-
-/**
- * Set the maximum dimensions for the paint blobs (rectangles)
- * 
- * @param width the maximum blob width
- * @param height the maximum blob height
- */
-RorschachPainter.prototype.setMaximumBlobDimensions = function(width, height) {
-    this.blobWidthMax = width;
-    this.blobHeightMax = height;
-}
-
-/**
- * Sets the range of ink to use (number of blobs to draw)
- * 
- * @param min the minimum number of blobs to be drawn
- * @param max the maximum number of blobs to be drawn
- */
-RorschachPainter.prototype.setInkAmountRange = function(min, max) {
-    this.inkAmountMin = min;
-    this.inkAmountMax = max;
-}
-
-/**
- * Paints the Rorschach image on the canvas
- * 
- * @param canvasId id for an HTML5 canvas element
- */
-RorschachPainter.prototype.paint = function(canvasId) {
-    
-    // Get the canvas context
-    var rorschachCanvas = document.getElementById(canvasId);
-    var context = rorschachCanvas.getContext('2d');
-    
-    // Grab a color from the palette
-    var baseColor = this.palette[randomInt(this.palette.length)];
-    
-    // Generate a random brush
-    var brush = new Brush(
-        randomRange(1, this.blobWidthMax), // Random blob width
-        randomRange(1, this.blobHeightMax), // Random blob height
-        baseColor.clone()); // Start with a base color from the palette
-    
-    // Pick a random point to draw first
-    var x = randomInt(rorschachCanvas.width / 2);
-    var y = randomInt(rorschachCanvas.height);
-
-    // Maximum range of values a color can stray
-    var COLOR_RANGE = Math.pow(8, 2); // Squared since the color distance algorithm does the same 
-
-    // The number of blobs to make
-    var inkAmount = randomRange(this.inkAmountMin, this.inkAmountMax);
-    
-    // Draw a bunch of random blobs
-    for(i =0; i < inkAmount; i++) {
-
-        // Draw that blob and mirror it!
-        context.fillStyle = brush.color.toString();
-        context.fillRect(x, y, brush.width, brush.height);
-        context.fillRect(rorschachCanvas.width - x, y, brush.width, brush.height);
-        
-        // Now it's time to prepare the next random location for the blob!
-        
-        // Generate a color near to the last one
-        brush.color.setRed(brush.color.red - 1 + (randomInt(100) % 3));
-        brush.color.setGreen(brush.color.green - 1 + (randomInt(100) % 3));
-        brush.color.setBlue(brush.color.blue - 1 + (randomInt(100) % 3));
-        
-        // Pick a random spot to draw the blob
-        x = (x - 4) + (randomInt(100) % 9);
-        y = (y - 4) + (randomInt(100) % 9);
-
-        // Pick random blob dimensions
-        brush.width = randomRange(1, this.blobWidthMax);
-        brush.height = randomRange(1, this.blobHeightMax);
-        
-        // Make sure the colors are near enough to the base color from the palette
-        if(brush.color.diff(baseColor) > COLOR_RANGE) {
-            brush.color = this.palette[randomInt(this.palette.length)].clone();
-        }
-
-        // Make sure the blob is inside the width of half the canvas
-        if(x < 0) {
-            x = randomInt(rorschachCanvas.width / 2);
-
-        } else if(x > rorschachCanvas.width / 2) {
-            x = randomInt(rorschachCanvas.width / 2);
-        }
-
-        // Make sure the blob is inside the height of the canvas
-        if(y < 0) {
-            y = randomInt(rorschachCanvas.height);
-
-        } else if(y > rorschachCanvas.height) {
-            y = randomInt(rorschachCanvas.height);
-        }
+  // Convenience methods to get and set pixels while we work
+  const get = (x, y) => {
+    if (x < 0 || x >= width || y < 0 || y >= height) {
+      return 0;
     }
+    return pixels[y][x];
+  };
+
+  const set = (x, y, value) => {
+    if (x >= 0 && x < width && y >= 0 && y < height) {
+      pixels[y][x] = value;
+    }
+  };
+
+  // Throw a bunch of seed blots onto the scene
+  let lastLocation = [randInt(0, width), randInt(0, height)];
+  for (let i = 0; i < blots; i++) {
+    const nextLocation = Math.random() < 0.9
+      ? lastLocation.map(v => v + (randInt(0, 50) - 25))
+      : [randInt(0, width), randInt(0, height)];
+    set(...nextLocation, randInt(1, Math.pow(iterations, 4)));
+    lastLocation = nextLocation;
+  }
+
+  // Blend each pixel into those around it several times
+  for (let i = 0; i < iterations; i++) {
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const self = get(x, y);
+        const n = get(x, y - 1);
+        const ne = get(x + 1, y - 1);
+        const e = get(x + 1, y);
+        const se = get(x + 1, y + 1);
+        const s = get(x, y + 1);
+        const sw = get(x - 1, y + 1);
+        const w = get(x - 1, y);
+        const nw = get(x - 1, y - 1);
+        set(x, y, (self + n + ne + e + se + s + sw + w + nw) / 9);
+      }
+    }
+  }
+
+  // Fade the edges
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (get(x, y) < iterations * 0.25) {
+        set(x, y, 0);
+      }
+    }
+  }
+
+  // Draw the ink blots to the canvas
+  const ctx = canvas.getContext('2d');
+  for (let y = 0; y < canvas.height; y++) {
+    for (let x = 0; x < canvas.width / 2; x++) {
+      const value = get(x, y);
+      ctx.fillStyle = `rgba(0, 0, 0, ${value / iterations})`;
+      ctx.fillRect(x, y, 1, 1);
+      ctx.fillRect(canvas.width - (x + 1), y, 1, 1);
+    }
+  }
+}
+
+function randInt (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
